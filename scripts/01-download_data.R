a27c2616d4361e8def133f2e8a44000102857dfe
Amie Liu <amieliu2121@gmail.com>
2024-01-16 21:17:52 +0000

    Update the download data file
    Save data to the raw data csv
    
    Rename the raw data csv

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,35 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the Open Data Toronto portal
+# Author: Amie Liu
+# Date: 16 January 2024
+# Contact: amie.liu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
 
 #### Workspace setup ####
+#install.packages("opendatatoronto")
+#install.packages("dplyr")
+#install.packages("readr")
+
 library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+library(readr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#### Download COVID-19 cases in Toronto data ####
+# Code from: https://open.toronto.ca/dataset/covid-19-cases-in-toronto/
+# Get all resources from this package
+resources <- list_package_resources("64b54586-6180-4485-83eb-81e8fae3b8fe")
 
+# Identify datastore resources in csv format
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
+# Load the COVID-19 cases.csv
+raw_case_data <- filter(datastore_resources, row_number()==2) |> get_resource()
+
+raw_case_data
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_case_data, "inputs/data/raw_data.csv")
 
-         
+# Rename the file for raw data
+file.rename("inputs/data/raw_data.csv", "inputs/data/raw_case_data.csv")
